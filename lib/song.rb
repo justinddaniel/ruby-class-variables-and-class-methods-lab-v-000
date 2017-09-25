fc366d44f60979644198b508abc1e769e3194901
Justin Daniel <justinddaniel@gmail.com>
2017-09-25 19:53:13 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,39 @@
+require 'pry'
+
+class Song
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  @@artist_count = {}
+  @@genre_count = {}
+  @@songs = []
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @@count += 1
+    @@genres.push(genre)
+    @@artists.push(artist)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@songs.push(name)
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    genres = @@genres.uniq
+  end
+
+  def self.artists
+    artists = @@artists.uniq
+  end
+
+  def self.genre_count
+    @@artists.each do |a|
+      @@genre_count[a] || += 1
+    end
+  end
+end
