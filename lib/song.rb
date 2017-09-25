a8789cf624df6d9c52b1576ef12fe12f8c4c3dac
Justin Daniel <justinddaniel@gmail.com>
2017-09-25 20:09:41 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,6 +28,17 @@ class Song
     artists = @@artists.uniq
   end
 
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |a|
+      if artist_count[a]
+        artist_count[a] += 1
+      else artist_count[a] = 1
+      end
+    end
+    artist_count
+  end
+
   def self.genre_count
     genre_count = {}
     @@genres.each do |g|
